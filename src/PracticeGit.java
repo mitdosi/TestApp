e8f0d590d9e1d081e4bfc2d104682866b53060ae
mit dosi <mdosi89@gmail.com>
2016-04-01 17:28:02 +0530

    initial commit

---
+++ src/PracticeGit.java
@@ -0,0 +1,15 @@
+
+public class PracticeGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//practice git
+		
+		PracticeGit2 p = new PracticeGit2();
+		p.setA(10);
+		p.setB(10);
+		System.out.println(p.comp(p.getA(), p.getB()));
+		
+	}
+
+}
---
+++ src/PracticeGit2.java
@@ -0,0 +1,32 @@
+
+public class PracticeGit2 {
+	int a;
+	int b;
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	
+	
+	boolean comp(int xy, int zx){
+		if(xy == zx){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+}
